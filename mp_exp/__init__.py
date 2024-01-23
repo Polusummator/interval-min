4c9db3fdd6a8c2671bdb4052fd0c70c0f56c3269
Gunter-Q12 <vvrudavskiy_2@edu.hse.ru>
2024-01-23 09:38:34 +0300

    Add 'convert_to_interval' to the interface of the library

--- mp_exp/__init__.py
+++ mp_exp/__init__.py
@@ -1,2 +1,2 @@
 from .ia_math_fun import exp, log, set_exp_taylor_terms_number, set_log_taylor_terms_number
-from .interval_arithmetics import Interval, set_precision
+from .interval_arithmetics import Interval, set_precision, convert_to_interval
