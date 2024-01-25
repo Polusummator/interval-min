d8d3189ac74b9fd2d99e42553954799a69c98cc2
Alexander Reznichenko <avreznichenko_2@edu.hse.ru>
2024-01-25 23:08:53 +0300

    Little optimization of MooreSkelboe

--- optimization_methods/MooreSkelboe.py
+++ optimization_methods/MooreSkelboe.py
@@ -19,15 +19,17 @@ class MooreSkelboe:
         while wid(self.interval_extension.evaluate(cells[0])) >= self.precision:
             widmax_variable = 0  # sympy.Symbol actually
             widmax = 0
-            for item in cells[0].items():
+            current_cell = cells[0]
+            for item in current_cell.items():
                 if wid(item[1]) > widmax:
                     widmax = wid(item[1])
                     widmax_variable = item[0]
-            widmax_interval = cells[0][widmax_variable]
-            new_cell_left = cells[0].copy()  # todo: copy?
-            new_cell_right = cells[0].copy()  # todo: copy?
-            new_interval_left = Interval(widmax_interval.a, mid(widmax_interval))
-            new_interval_right = Interval(mid(widmax_interval), widmax_interval.b)
+            widmax_interval = current_cell[widmax_variable]
+            new_cell_left = current_cell.copy()
+            new_cell_right = current_cell.copy()
+            middle = mid(widmax_interval)
+            new_interval_left = Interval(widmax_interval.a, middle)
+            new_interval_right = Interval(middle, widmax_interval.b)
             new_cell_left[widmax_variable] = new_interval_left
             new_cell_right[widmax_variable] = new_interval_right
             cells.pop(0)
