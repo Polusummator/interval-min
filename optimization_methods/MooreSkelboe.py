5dada272580f152133f8a6ff2ad194bb88810627
Gunter-Q12 <vvrudavskiy_2@edu.hse.ru>
2024-01-26 19:10:18 +0300

    Move max_wid_variable search into a separate function

--- optimization_methods/MooreSkelboe.py
+++ optimization_methods/MooreSkelboe.py
@@ -14,15 +14,10 @@ class MooreSkelboe:
     def calculate(self):
         cells = self._get_sorted_list(self.extremum_type)
         cells.add(self.func_args)
-        while wid(self.interval_extension.evaluate(cells[0])) >= self.answer_precision:
-            maxwid_variable = 0  # sympy.Symbol actually
-            maxwid = 0
-            for item in cells[0].items():
-                if wid(item[1]) > maxwid:
-                    maxwid = wid(item[1])
-                    maxwid_variable = item[0]
 
-            new_cells = self._split_domain(cells[0], maxwid_variable)
+        while wid(self.interval_extension.evaluate(cells[0])) >= self.answer_precision:
+            max_wid_variable = self._get_max_wid_variable(cells[0])
+            new_cells = self._split_domain(cells[0], max_wid_variable)
             cells.pop(0)
             for cell in new_cells:
                 cells.add(cell)
@@ -50,3 +45,12 @@ class MooreSkelboe:
         if self.extremum_type == "min":
             return SortedList(key=lambda x: self.interval_extension.evaluate(x).a)
         return SortedList(key=lambda x: -self.interval_extension.evaluate(x).b)
+
+    def _get_max_wid_variable(self, domain: dict[str, Interval]) -> str:
+        max_wid_variable = 0  # sympy.Symbol actually
+        max_wid = 0
+        for item in domain.items():
+            if wid(item[1]) > max_wid:
+                max_wid = wid(item[1])
+                max_wid_variable = item[0]
+        return max_wid_variable
