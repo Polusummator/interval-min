9bb946ca36e2141de45f424819515caabd9d83fd
Gunter-Q12 <vvrudavskiy_2@edu.hse.ru>
2024-01-26 18:46:27 +0300

    Move domain splitting into a separate function

--- optimization_methods/MooreSkelboe.py
+++ optimization_methods/MooreSkelboe.py
@@ -17,22 +17,27 @@ class MooreSkelboe:
             cells = SortedList(key=lambda x: -self.interval_extension.evaluate(x).b)
         cells.add(self.func_args)
         while wid(self.interval_extension.evaluate(cells[0])) >= self.precision:
-            widmax_variable = 0  # sympy.Symbol actually
-            widmax = 0
-            current_cell = cells[0]
-            for item in current_cell.items():
-                if wid(item[1]) > widmax:
-                    widmax = wid(item[1])
-                    widmax_variable = item[0]
-            widmax_interval = current_cell[widmax_variable]
-            new_cell_left = current_cell.copy()
-            new_cell_right = current_cell.copy()
-            middle = mid(widmax_interval)
-            new_interval_left = Interval(widmax_interval.a, middle)
-            new_interval_right = Interval(middle, widmax_interval.b)
-            new_cell_left[widmax_variable] = new_interval_left
-            new_cell_right[widmax_variable] = new_interval_right
+            maxwid_variable = 0  # sympy.Symbol actually
+            maxwid = 0
+            for item in cells[0].items():
+                if wid(item[1]) > maxwid:
+                    maxwid = wid(item[1])
+                    maxwid_variable = item[0]
+
+            new_cells = self._split_domain(cells[0], maxwid_variable)
             cells.pop(0)
-            cells.add(new_cell_left)
-            cells.add(new_cell_right)
+            for cell in new_cells:
+                cells.add(cell)
         return cells[0]
+
+    def _split_domain(self, domain: dict[str, Interval], variable: str) -> list[dict]:
+        split_interval = domain[variable]
+        new_interval_left = Interval(split_interval.a, mid(split_interval))
+        new_interval_right = Interval(mid(split_interval), split_interval.b)
+
+        left_half = domain.copy()  # todo: copy?
+        right_half = domain.copy()  # todo: copy?
+        left_half[variable] = new_interval_left
+        right_half[variable] = new_interval_right
+
+        return [left_half, right_half]
