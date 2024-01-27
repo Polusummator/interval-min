c8cc454c755e095099f7501da07f5a2419e38e94
Gunter-Q12 <vvrudavskiy_2@edu.hse.ru>
2024-01-27 12:59:33 +0300

    Fix code style

--- optimization_methods/MooreSkelboe.py
+++ optimization_methods/MooreSkelboe.py
@@ -12,7 +12,7 @@ class MooreSkelboe:
         self.extremum_type = extremum_type
 
     def calculate(self):
-        cells = self._get_sorted_list(self.extremum_type)
+        cells = self._get_sorted_list()
         cells.add(self.func_args)
 
         while wid(self.interval_extension.evaluate(cells[0])) >= self.answer_precision:
@@ -36,12 +36,12 @@ class MooreSkelboe:
 
         return [left_half, right_half]
 
-    def _set_precision(self, interval):
+    def _set_precision(self, interval: Interval):
         if wid(interval) <= self.calculation_precision * 2:
             set_precision(-self.calculation_precision.as_tuple().exponent + 1)
             self.calculation_precision /= 10
 
-    def _get_sorted_list(self, extremum_type):
+    def _get_sorted_list(self):
         if self.extremum_type == "min":
             return SortedList(key=lambda x: self.interval_extension.evaluate(x).a)
         return SortedList(key=lambda x: -self.interval_extension.evaluate(x).b)
