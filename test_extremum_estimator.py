3f67ec2aa7ea3c1432870dab85718a6813ce5b8a
Konstantin Leontev <kvleontev_1@edu.hse.ru>
2024-01-29 20:49:04 +0300

    dungeon tests

--- test_extremum_estimator.py
+++ test_extremum_estimator.py
@@ -1,82 +1,62 @@
 from decimal import Decimal
 from extremum_estimator import get_extremum_estimation
 import unittest
+import csv
 import mp_exp.interval_arithmetics as ia
 
 
 class TestExtr(unittest.TestCase):
-    def test_base(self):
-        f = "x**2 + 1"
-        base = "0."
-        for i in range(200):
-            epsilon = Decimal(base + '1')
-            res = get_extremum_estimation(
-                f, {"x": ia.Interval(Decimal("-10"), Decimal("100"))}, "min", epsilon
-            )
-            self.assertTrue(abs(Decimal(res) - Decimal("1")) < epsilon)
-            base += '0'
+    def test(self):
+        print("\n\n\n")
+        with open('test_extremum_estimator.csv', 'r', encoding='utf-8') as csv_file:
+            caption = ""
+            for num, row in enumerate(csv.reader(csv_file, delimiter=',')):
+                flag = True
+                if not num:
+                    caption = row
+                    continue
+                else:
+                    f = ""
+                    ans = ""
+                    d = dict()
+                    extr = ""
+                    for nm, data in enumerate(row):
+                        if caption[nm] == "Function":
+                            f = data
+                        elif caption[nm] == "Extr":
+                            extr = data
+                        elif caption[nm] == "Intervals":
+                            s = data.split(" ")
+                            for x in s:
+                                ds = x.split(":")
+                                dx = ds[1].split(",")
+                                a = Decimal(dx[0][1:])
+                                b = Decimal(dx[1][:-1])
+                                d = {ds[0]: ia.Interval(a, b)}
+                        elif caption[nm] == "Correct":
+                            ans = Decimal(data)
+                        print(f'{caption[nm]}: {data}')
+                    base = "0."
+                    for i in range(5):
+                        epsilon = Decimal(base + '1')
+                        try:
+                            res = get_extremum_estimation(f, d, extr, epsilon)
+                            self.assertTrue(abs(Decimal(res) - ans) < epsilon)
+                        except:
+                            flag = False
+                            break
+                        base += '0'
+                    if flag:
+                        print(f"TEST PASSED\n")
+                    else:
+                        print(f"TEST FAILED\n")
 
-    def test_border(self):
-        f = "x"
-        base = "0."
-        for i in range(100):
-            epsilon = Decimal(base + '1')
-            res = get_extremum_estimation(
-                f, {"x": ia.Interval(Decimal("-10000"), Decimal("100"))}, "min", epsilon
-            )
-            self.assertTrue(abs(Decimal(res) - Decimal("-10000")) < epsilon)
-            base += '0'
-        f = "x**3"
-        base = "0."
-        for i in range(10):
-            epsilon = Decimal(base + '1')
-            res = get_extremum_estimation(
-                f, {"x": ia.Interval(Decimal("100"), Decimal("200"))}, "min", epsilon
-            )
-            self.assertTrue(abs(Decimal(res) - Decimal("1000000")) < epsilon)
-            base += '0'
 
-    def test_exp(self):
-        f = "exp(x)"
-        base = "0."
-        for i in range(10):
-            epsilon = Decimal(base + '1')
-            res = get_extremum_estimation(
-                f, {"x": ia.Interval(Decimal('1'), Decimal('10'))}, "min", epsilon
-            )
-            self.assertTrue(abs(Decimal(res) - Decimal("2.7182818284590452353602874713526624")) < epsilon)
-            base += '0'
-
-    def test_TL_moment(self):
-        f = "x**2 + x + 1"
-        base = "0."
-        for i in range(10):
-            epsilon = Decimal(base + '1')
-            res = get_extremum_estimation(
-                f, {"x": ia.Interval(Decimal("-100"), Decimal("100"))}, "min", epsilon
-            )
-            self.assertTrue(abs(Decimal(res) - Decimal("0.75")) < epsilon)
-            base += '0'
-
-    def test_dot(self):
-        f = "1"
-        base = "0."
-        for i in range(10):
-            epsilon = Decimal(base + '1')
-            res = get_extremum_estimation(
-                f, {"x": ia.Interval(Decimal("-10"), Decimal("100"))}, "min", epsilon
-            )
-            self.assertTrue(abs(Decimal(res) - Decimal("1")) < epsilon)
-            base += '0'
 
 
 def suite():
     s = unittest.TestSuite()
-    s.addTest(TestExtr('test_base'))
-    s.addTest(TestExtr('test_border'))
-    s.addTest(TestExtr('test_exp'))
-    s.addTest(TestExtr('test_TL_moment'))
-    s.addTest(TestExtr('test_dot'))
+    s.addTest(TestExtr('test'))
     return s
 
 
