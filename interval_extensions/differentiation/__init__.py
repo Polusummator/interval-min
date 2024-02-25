d779ecd031f2ef735518f4f886c204b70fa11ff8
Gunter-Q12 <vvrudavskiy_2@edu.hse.ru>
2024-02-25 21:32:18 +0300

    Add forward mode differentiaion

--- interval_extensions/differentiation/__init__.py
+++ interval_extensions/differentiation/__init__.py
@@ -1,1 +1,2 @@
-from .sympy_diffentiation import get_gradient_evaluator
+# from .sympy_diffentiation import get_gradient_evaluator
+from .forward_differentiation import get_gradient_evaluator
---
+++ interval_extensions/differentiation/forward_differentiation.py
@@ -0,0 +1,91 @@
+from mp_exp import Interval, exp, log, factorial
+from decimal import Decimal
+
+from function_evaluator import FunctionEvaluator
+
+
+class DerivativePair:
+    def __init__(self, value, derivative=Interval(0, 0)):
+        self.value = Interval.to_interval(value)
+        self.derivative = Interval.to_interval(derivative)
+
+    @classmethod
+    def to_pair(cls, other):
+        if type(other) is Decimal or type(other) is Interval:
+            return DerivativePair(other)
+        elif type(other) is int or type(other) is float:
+            v = Decimal(other)
+            return DerivativePair(v)
+        else:
+            return other
+
+    def __neg__(self):
+        value = -self.value
+        derivative = -self.derivative
+        return DerivativePair(value, derivative)
+
+    def __add__(self, other):
+        other = DerivativePair.to_pair(other)
+        value = self.value + other.value
+        derivative = self.derivative + other.derivative
+        return DerivativePair(value, derivative)
+
+    def __sub__(self, other):
+        other = DerivativePair.to_pair(other)
+        value = self.value - other.value
+        derivative = self.derivative - other.derivative
+        return DerivativePair(value, derivative)
+
+    def __mul__(self, other):
+        other = DerivativePair.to_pair(other)
+        value = self.value * other.value
+        derivative = self.derivative * other.value + self.value * other.derivative
+        return DerivativePair(value, derivative)
+
+    def __truediv__(self, other):
+        other = DerivativePair.to_pair(other)
+        value = self.value / other.value
+        derivative = (self.derivative * other.value - self.value * other.derivative) / (other.value ** 2)
+        return DerivativePair(value, derivative)
+
+    def __pow__(self, power):
+        value = self.value ** power
+        derivative = self.value ** (power - 1) * self.derivative * (power)
+        return DerivativePair(value, derivative)
+
+
+def pair_log(pair: DerivativePair):
+    value = log(pair.value)
+    derivative = pair.derivative / pair.value
+    return DerivativePair(value, derivative)
+
+
+def pair_exp(pair: DerivativePair):
+    value = exp(pair.value)
+    derivative = pair.derivative * exp(pair.value)
+    return DerivativePair(value, derivative)
+
+
+def pair_factorial(pair: DerivativePair):
+    raise RuntimeError("Cannot differentiate factorial")
+
+
+derivative_pair_operations = {'exp': pair_exp, 'log': pair_log, 'factorial': pair_factorial}
+
+
+class ForwardGradientEvaluator:
+    def __init__(self, variables: dict, expr: str):
+        self.gradient_evaluator = FunctionEvaluator(variables, expr, derivative_pair_operations)
+
+    def evaluate(self, variables: dict):
+        gradient = dict()
+        derivative_pairs = {variable: DerivativePair(interval) for variable, interval in variables.items()}
+        for variable, interval in variables.items():
+            derivative_pairs[variable]= DerivativePair(interval, Interval.to_interval(1))
+            gradient[variable] = self.gradient_evaluator.evaluate(derivative_pairs).derivative
+            derivative_pairs[variable] = DerivativePair(interval, Interval.to_interval(0))
+        return gradient
+
+
+def get_gradient_evaluator(variables: dict, expr: str):
+    return ForwardGradientEvaluator(variables, expr)
