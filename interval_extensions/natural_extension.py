af43ce923064ff3151cfb5c307f33d4bb1f1cd3b
Gunter-Q12 <vvrudavskiy_2@edu.hse.ru>
2024-04-22 11:25:59 +0300

    Fix 'no attribute .a' errror
    The error has occured because sympy optimizes expressions like x/x to
    always return dot value 1

--- interval_extensions/natural_extension.py
+++ interval_extensions/natural_extension.py
@@ -1,6 +1,6 @@
 from .function_evaluator import FunctionEvaluator
 from .interval_extension import IntervalExtension
-from mp_exp import exp, log, factorial
+from mp_exp import exp, Interval, log, factorial
 
 
 interval_operations = {'exp': exp, 'log': log, 'factorial': factorial}
@@ -11,4 +11,4 @@ class NaturalExtension(IntervalExtension):
         self.extension = FunctionEvaluator(expr, variable_names, interval_operations)
 
     def evaluate(self, variables):
-        return self.extension.evaluate(variables)
+        return Interval.to_interval(self.extension.evaluate(variables))
