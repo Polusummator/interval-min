274e4125f498b62ad02b77db6c75368364149c66
Леонтьев Константин Валерьевич <kvleontev_1@edu.hse.ru>
2024-04-24 01:19:46 +0300

    added extra tests, several args, readme

--- compare.py
+++ compare.py
@@ -1,20 +1,16 @@
 import decimal
 import time
-import unittest
 from dataclasses import dataclass
 from decimal import Decimal
-from itertools import product
 import timeout_decorator
-import pandas as pd
 import numpy as np
-from parameterized import parameterized
-import re
-from random import *
+from random import randint, sample
 import matplotlib.pyplot as plt
-from extremum_estimator import get_extremum_estimation, EXTENSIONS, METHODS, DIFFS
+# from optimization_methods import MooreSkelboe_no_mods
+from extremum_estimator import get_extremum_estimation, EXTENSIONS, DIFFS
 from mp_exp import Interval
 
-TIMEOUT = 5
+TIMEOUT = 10
 amount = 20
 a = []
 
@@ -37,40 +33,53 @@ koefs = [str(i) for i in range(-100, 101)]
 degs = [i for i in range(0, 16)]
 
 
-def generate():
+def generate(argzz):
     f = ""
-    m = randint(0, 15)
-    m += 1
-    koefk = sample(koefs, m)
-    degk = sample(degs, m)
-    degk = sorted(degk)
-    degk.reverse()
-    degk = [str(i) for i in degk]
-    # print(koefk, degk)
-    f += koefk[0] + " * x**(" + degk[0] + ")"
-    for i in range(1, m):
-        if koefk[i] == '0':
-            continue
-        if degk[i] == '1':
-            if int(koefk[i]) > 0:
-                f += " + " + koefk[i] + " * x"
-            else:
-                f += " - " + str(-int(koefk[i])) + " * x"
-            continue
-        if degk[i] == '0':
-            if int(koefk[i]) > 0:
-                f += " + " + koefk[i]
-            else:
-                f += " - " + str(-int(koefk[i]))
-            continue
-        if int(koefk[i]) > 0:
-            f += " + " + koefk[i] + " * x**(" + degk[i] + ")"
+    for arg in range(argzz):
+
+        m = randint(0, 15)
+        m += 1
+        koefk = sample(koefs, m)
+        degk = sample(degs, m)
+        degk = sorted(degk)
+        degk.reverse()
+        degk = [str(i) for i in degk]
+        if arg == 0:
+            c_arg = 'x'
+        elif arg == 1:
+            c_arg = 'y'
+            if len(f) != 0:
+                f += ' + '
         else:
-            f += " - " + str(-int(koefk[i])) + " * x**(" + degk[i] + ")"
+            c_arg = 'z'
+            if len(f) != 0:
+                f += ' + '
+        f += "(" + koefk[0] + f") * {c_arg}**(" + degk[0] + ")"
+        for i in range(1, m):
+            if koefk[i] == '0':
+                continue
+            if degk[i] == '1':
+                if int(koefk[i]) > 0:
+                    f += " + " + koefk[i] + f" * {c_arg}"
+                else:
+                    f += " - " + str(-int(koefk[i])) + f" * {c_arg}"
+                continue
+            if degk[i] == '0':
+                if int(koefk[i]) > 0:
+                    f += " + " + koefk[i]
+                else:
+                    f += " - " + str(-int(koefk[i]))
+                continue
+            if int(koefk[i]) > 0:
+                f += " + " + koefk[i] + f" * {c_arg}**(" + degk[i] + ")"
+            else:
+                f += " - " + str(-int(koefk[i])) + f" * {c_arg}**(" + degk[i] + ")"
+    # print(f)
     return f
 
 
 values = []
+# values_no_mods = []
 
 
 def do_it():
@@ -79,15 +88,38 @@ def do_it():
 
     for _ in range(amount):
         i = dict()
-        i['x'] = Interval(-10, 10)
-        f = generate()
+        args = randint(1, 3)
+        if args == 1:
+            i['x'] = Interval(-10, 10)
+        elif args == 2:
+            i['x'] = Interval(-10, 10)
+            i['y'] = Interval(-10, 10)
+        else:
+            i['x'] = Interval(-10, 10)
+            i['y'] = Interval(-10, 10)
+            i['z'] = Interval(-10, 10)
+        f = generate(args)
         test = TestData(name='base', func=f,
                         answer=decimal.Decimal(0), intervals=i,
                         precision=decimal.Decimal('0.01'))
         x = []
+        # y = []
         # print(f)
+
         for extension in EXTENSIONS.keys():
             for diff in DIFFS.keys():
+                # try:
+                #     t = time.time()
+                #     result = limited_extremum_estimation_execution(test, extension, "moore_skelboe_no_mods", diff)
+                #     real_time = time.time() - t
+                #     y.append(real_time)
+                #     print(f"method: moore_skelboe_no_mods\nextension: {extension}\ndiff: {diff}\nstatus: passed\ntest name: {index}\ntime: {real_time}")
+                #     print()
+                # except:
+                #     y.append(None)
+                #     print(f"method: moore_skelboe_no_mods\nextension: {extension}\ndiff: {diff}\nstatus: failed\ntest name: {index}\ntime: infinity")
+                #     print()
+                # index += 1
                 try:
                     t = time.time()
                     result = limited_extremum_estimation_execution(test, extension, method, diff)
@@ -101,14 +133,17 @@ def do_it():
                     # print()
                 index += 1
         values.append(x)
+        # values_no_mods.append(y)
 
 
 do_it()
 max_time = [-1] * (len(EXTENSIONS) * len(DIFFS))
 min_time = [10000] * (len(EXTENSIONS) * len(DIFFS))
 middle = [[amount, 0] for _ in range(len(EXTENSIONS) * len(DIFFS))]
+# max_time_no_mods = [-1] * (len(EXTENSIONS) * len(DIFFS))
+# min_time_no_mods = [10000] * (len(EXTENSIONS) * len(DIFFS))
+# middle_no_mods = [[amount, 0] for _ in range(len(EXTENSIONS) * len(DIFFS))]
 for x in values:
-    # print(x)
     for i in range(0, len(EXTENSIONS) * len(DIFFS)):
         if x[i] is None:
             middle[i][0] -= 1
@@ -116,44 +151,55 @@ for x in values:
         max_time[i] = max(max_time[i], x[i])
         min_time[i] = min(min_time[i], x[i])
         middle[i][1] += x[i]
-# print(middle)
+# for x in values_no_mods:
+#     for i in range(0, len(EXTENSIONS) * len(DIFFS)):
+#         if x[i] is None:
+#             middle_no_mods[i][0] -= 1
+#             continue
+#         max_time_no_mods[i] = max(max_time_no_mods[i], x[i])
+#         min_time_no_mods[i] = min(min_time_no_mods[i], x[i])
+#         middle_no_mods[i][1] += x[i]
 average = []
 failures = []
+# average_no_mods = []
+# failures_no_mods = []
 
 for x in middle:
-    # print(x[1] / x[0])
     average.append(x[1] / x[0])
     failures.append(amount - x[0])
+# for x in middle_no_mods:
+#     average_no_mods.append(x[1] / x[0])
+#     failures_no_mods.append(amount - x[0])
 
-objects = [x + '\n' + y for x in EXTENSIONS for y in DIFFS]
+objects = [x[0] + '_' + y[0:2] for x in EXTENSIONS for y in DIFFS]  # * 2
 y_pos = np.arange(len(objects))
 
 plt.subplot(221)
 plt.bar(y_pos, average, align='center', alpha=0.5)
 plt.xticks(y_pos, objects)
 plt.ylabel('time')
-plt.xticks(rotation=315)
+# plt.xticks(rotation=315)
 plt.title('Average execution time')
 
 plt.subplot(222)
-plt.bar(y_pos, failures, align='center', alpha=0.5)
+plt.bar(y_pos, failures, align='center', alpha=0.5, color='red')
 plt.xticks(y_pos, objects)
 plt.ylabel('amount')
-plt.xticks(rotation=315)
+# plt.xticks(rotation=315)
 plt.title('Amount of failed tests')
 
 plt.subplot(223)
-plt.bar(y_pos, max_time, align='center', alpha=0.5)
+plt.bar(y_pos, max_time, align='center', alpha=0.5, color='orange')
 plt.xticks(y_pos, objects)
 plt.ylabel('time')
-plt.xticks(rotation=315)
+# plt.xticks(rotation=315)
 plt.title('Max execution time')
 
 plt.subplot(224)
-plt.bar(y_pos, min_time, align='center', alpha=0.5)
+plt.bar(y_pos, min_time, align='center', alpha=0.5, color='green')
 plt.xticks(y_pos, objects)
 plt.ylabel('time')
-plt.xticks(rotation=315)
+# plt.xticks(rotation=315)
 plt.title('Min execution time')
 
 plt.show()
--- optimization_methods/__init__.py
+++ optimization_methods/__init__.py
@@ -1,1 +1,2 @@
 from .MooreSkelboe import MooreSkelboe
+from .MooreSkelboe_no_mods import MooreSkelboe_no_mods
