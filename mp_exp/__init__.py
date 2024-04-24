0afcb548b2b4bb58454aa15bd305db0e8cd851b0
Gunter-Q12 <vvrudavskiy_2@edu.hse.ru>
2024-04-24 21:03:55 +0300

    Fix exp and log implementations

--- mp_exp/__init__.py
+++ mp_exp/__init__.py
@@ -1,3 +1,4 @@
 from .ia_math_fun import factorial
-from .interval_arithmetics import Interval, set_precision, intersect
+from .interval_arithmetics import (Interval, set_precision, intersect,
+                                   c_minf, c_mtwo, c_mone, c_zero, c_one, c_two, c_inf)
 from .ia_math_fun_standard import exp, log
--- mp_exp/ia_math_fun_standard.py
+++ mp_exp/ia_math_fun_standard.py
@@ -1,34 +1,78 @@
 from decimal import Decimal, getcontext, ROUND_DOWN, ROUND_UP
+
+import mp_exp as ia
 from mp_exp import Interval, set_precision
 
 
 def exp(x: Interval) -> Interval:
     precision = getcontext().prec
-    lower_bound = _precise_calc(x.a, lambda a: a.exp(), precision, ROUND_DOWN) if x.a != 0 else 1
-    upper_bound = _precise_calc(x.b, lambda a: a.exp(), precision, ROUND_UP) if x.b != 0 else 1
+    is_edge_case, lower_bound = _exp_edge_cases_check(x.a)
+    if not is_edge_case:
+        lower_bound = _precise_calc(x.a, lambda a: a.exp(), precision, ROUND_DOWN)
+
+    is_edge_case, upper_bound = _exp_edge_cases_check(x.b)
+    if not is_edge_case:
+        upper_bound = _precise_calc(x.b, lambda a: a.exp(), precision, ROUND_UP)
 
     return Interval(lower_bound, upper_bound)
+
+
+def _exp_edge_cases_check(x: Decimal) -> tuple[bool, Decimal]:
+    """Returns if edge case is present for exp function and result value"""
+
+    if x == ia.c_zero:
+        return True, ia.c_one
+    if x == ia.c_minf:
+        return True, ia.c_zero
+    if x == ia.c_inf:
+        return True, ia.c_inf
+    return False, ia.c_zero
 
 
 def log(x: Interval) -> Interval:
-    if x.a <= 0:
-        raise ValueError("Log cannot be applied to an interval containing values less or equal to zero")
-
     precision = getcontext().prec
-    lower_bound = _precise_calc(x.a, lambda a: a.ln(), precision, ROUND_DOWN) if x.a != 1 else 0
-    upper_bound = _precise_calc(x.b, lambda a: a.ln(), precision, ROUND_UP) if x.b != 1 else 0
+    is_edge_case, lower_bound = _log_edge_cases_check(x.a)
+    if not is_edge_case:
+        lower_bound = _precise_calc(x.a, lambda a: a.ln(), precision, ROUND_DOWN)
+
+    is_edge_case, upper_bound = _log_edge_cases_check(x.b)
+    if not is_edge_case:
+        upper_bound = _precise_calc(x.b, lambda a: a.ln(), precision, ROUND_UP)
 
     return Interval(lower_bound, upper_bound)
+
+
+def _log_edge_cases_check(x: Decimal) -> tuple[bool, Decimal]:
+    """Returns if edge case is present for log function and result value"""
+
+    if x < ia.c_zero:
+        raise ValueError("Log cannot be applied to an interval containing values less than zero")
+    if x == ia.c_zero:
+        return True, ia.c_minf
+    if x == ia.c_one:
+        return True, ia.c_zero
+    if x == ia.c_inf:
+        return True, ia.c_inf
+    return False, ia.c_zero
 
 
 def _precise_calc(x: Decimal, func, precision: int, rounding) -> Decimal:
     precision_value = Decimal("0." + "0" * (precision - 1) + "1")
     extra = 3
+
+    # correctly rounded result: repeatedly increase precision by 3
+    # until we get an unambiguously roundable result
     while True:
         set_precision(precision + extra)
         estimation = func(x)
         if -estimation.as_tuple().exponent >= precision and estimation % precision_value != 0:
-            estimation.quantize(Decimal(precision_value), rounding=rounding)
+            estimation = _round(estimation, rounding, precision_value)
             set_precision(int(precision))
             return estimation
         extra += 3
+
+
+def _round(x: Decimal, rounding, precision_value: Decimal) -> Decimal:
+    if x < ia.c_zero:
+        rounding = ROUND_UP if rounding == ROUND_DOWN else ROUND_DOWN
+    return x.quantize(precision_value, rounding=rounding)
