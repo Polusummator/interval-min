6dd21ff44bdad33c892e68467b41673b6f1d6b89
Gunter-Q12 <vvrudavskiy_2@edu.hse.ru>
2024-04-24 12:47:34 +0300

    Make FuncitonEvaluator ignore varialbe order in dict

--- interval_extensions/function_evaluator.py
+++ interval_extensions/function_evaluator.py
@@ -3,7 +3,14 @@ import sympy
 
 class FunctionEvaluator:
     def __init__(self, expr: str, variable_names, operations: dict):
-        self.function = sympy.lambdify(list(variable_names), expr, modules=operations)
+        self.variable_names = list(variable_names)  # saved to preserve order of values
+        self.function = sympy.lambdify(self.variable_names, expr, modules=operations)
 
     def evaluate(self, variables: dict):
-        return self.function(*list(variables.values()))
+        """
+        Get result of function evaluation.
+
+        Order of variables in the dict is ignored.
+        """
+
+        return self.function(*[variables[variable_name] for variable_name in self.variable_names])
